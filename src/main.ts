936627c03ad43760c144f7a2846cf372e3c1f7c4
Jhon Baez <jhonbaez11@hotmail.com>
2026-08-10 10:07:38 -0500

    actualizacion

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,3 @@ import { appConfig } from './app/app.config';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
-
-/*import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { provideRouter } from '@angular/router';
-import { app_routes } from './app/app.routes';
-
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(app_routes)
-  ]
-});*/
